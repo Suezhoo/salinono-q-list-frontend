1aa90da302d12dfe1178bf98b8af79374963c072
Suezhoo <suezhoo@outlook.com>
2022-06-12 00:40:15 +0200

    Changed view, no longer able to input

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 window.onload = async function () {
-    // await addToQueue("Gamer 6");
-    initEventListeners();
     await getQueuers();
     // await removeFromQueue("Gamer 7");
 };
@@ -11,16 +9,6 @@ let AMOUNT; // Stores amount of current queuers
 setInterval(function () {
     getQueuers();
 }, 30000);
-
-function initEventListeners() {
-    const button_join_queue = document.querySelector("[data-q='button_join']");
-    button_join_queue.addEventListener("click", () => {
-        const twitch_name = document.querySelector("[data-q='twitch_name']").value;
-        const gamertag = document.querySelector("[data-q='gamertag']").value;
-
-        addToQueue(twitch_name, gamertag);
-    });
-}
 
 async function getQueuers() {
     // DOM
@@ -46,41 +34,10 @@ async function getQueuers() {
                     if (index == 2) element.innerHTML += `<hr>`;
                     element.innerHTML += `<div id="queuer">
                                             <p>${index + 1}</p>
-                                            <p>${queuer.twitch_name}</p>
-                                            <p>${queuer.gamertag}</p>
-                                            <a href="#" onclick="removeFromQueue('${queuer.twitch_name}')" href="#" class="remove-from-q">&chi;</a>
+                                            <p>${queuer.name}</p>
+                                            <a href="#" onclick="removeFromQueue('${queuer.name}')" href="#" class="remove-from-q">&chi;</a>
                                             </div>`;
                 });
-            }
-        });
-}
-
-async function addToQueue(twitch_name, gamertag) {
-    // Make gamertag param optional
-    if (gamertag === undefined || gamertag === "") gamertag = twitch_name;
-
-    const data = { twitch_name, gamertag };
-
-    await fetch("https://sali-q-list.herokuapp.com/queuers/add", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(data),
-    })
-        .then((res) => res.json())
-        .then((data) => {
-            const element = document.querySelector("[data-q='join_queue_error']");
-            if (data.error) {
-                element.style.display = "flex";
-                element.innerHTML = `${data.value}`;
-                if (element.classList.contains("success")) element.classList.replace("success", "error");
-                element.classList.add("error");
-            } else {
-                element.style.display = "flex";
-                element.innerHTML = "Joined the queue";
-                if (element.classList.contains("error")) element.classList.replace("error", "success");
-                element.classList.add("success");
-                // Reload page
-                window.location.reload();
             }
         });
 }
